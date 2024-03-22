ccd4835e053d99231cb29fc3eec2b2d8b4c6726d
G00422221@atu.ie <G00422221@atu.ie>
2024-03-22 12:28:53 +0000

    Created classes

--- src/main/java/ie/atu/Teams.java
+++ src/main/java/ie/atu/Teams.java
@@ -7,23 +7,4 @@ import java.sql.Statement;
 
 public class Teams {
     public static void main(String[] args) {
-        String selectSQL = "SELECT u.username, u.password, e.email " +
-                "FROM users u " +
-                "JOIN emails e ON u.id = e.user_id";
-
-        try (Connection connection = DatabaseUtils.getConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(selectSQL)) {
-
-            while (resultSet.next()) {
-                String user = resultSet.getString("username");
-                String pass = resultSet.getString("password");
-                String email = resultSet.getString("email");
-
-                System.out.println("Username: " + user + ", Password: " + pass + ", Email: " + email);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 }
