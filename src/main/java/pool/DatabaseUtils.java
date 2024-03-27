9046e926d90b89803abcbc5477e2cf25db4d637a
AlexP8on <118891272+AlexP8on@users.noreply.github.com>
2024-03-27 11:55:55 +0000

    Updated Database

--- src/main/java/pool/DatabaseUtils.java
+++ src/main/java/pool/DatabaseUtils.java
@@ -6,7 +6,7 @@ import com.mysql.cj.jdbc.MysqlDataSource;
 
 public class DatabaseUtils {
     //later we will look at storing this type of data in a better location like a properties file
-    private static final String URL = "jdbc:mysql://localhost:3306/Dump20240322";
+    private static final String URL = "jdbc:mysql://localhost:3306/PremDatabase";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "password";
     private static final DataSource dataSource;
