265a8432d004dda8e97004969c3084afba831769
ThomasGallagher1 <g00415457@atu.ie>
2024-03-28 14:14:24 +0000

    added rest of stats about titles to each premier league team

--- src/main/java/ie/atu/Stats.java
+++ src/main/java/ie/atu/Stats.java
@@ -5,6 +5,25 @@ public class Stats {
         PremTable[] premtable = new PremTable[]
                 {
                         new Titles("Arsenal", 1, 3,0,14,2),
+                        new Titles("Liverpool", 2,1,6,8,10),
+                        new Titles("Manchester City", 3,6,1,7,8),
+                        new Titles("Aston villa", 4,0,1,7,8),
+                        new Titles("Tottenham Hotspur", 5,0,0,8,4),
+                        new Titles("Manchester United", 6,13,3,12,6),
+                        new Titles("West Ham United", 7,0,0,3,0),
+                        new Titles("Brighton", 8,0,0,0,0),
+                        new Titles("Wolves", 9,0,0,4,2),
+                        new Titles("Newcastle United", 10,0,0,6,0),
+                        new Titles("Chelsea", 11,5,2,8,5),
+                        new Titles("Fulham", 12,0,0,0,0),
+                        new Titles("Bournemouth", 13,0,0,0,0),
+                        new Titles("Crystal Palace", 14,0,0,0,0),
+                        new Titles("Brentford", 15,0,0,0,0),
+                        new Titles("Everton", 16,0,0,5,0),
+                        new Titles("Luton Town", 17,0,0,0,1),
+                        new Titles("Nottingham forrest", 18,0,2,2,4),
+                        new Titles("Burnley", 19,0,0,1,0),
+                        new Titles("Sheffield United", 20,0,0,4,0),
                 };
         for(PremTable premtables : premtable){
             System.out.println("Position: " + premtables.getLeaguePosition());
